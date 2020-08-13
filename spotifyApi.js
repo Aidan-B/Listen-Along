227db61eaa2328250b0e6401ad8e13ef0e2c2d71
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2020-08-13 00:34:23 -0400

    Decode status codes

--- spotifyApi.js
+++ spotifyApi.js
@@ -275,3 +275,51 @@ module.exports.start = async function (access_token, uri) {
         req.end();
     })
 }
+
+
+module.exports.getStatusCode = function(status_code) {
+    switch (status_code) {
+        case 200:
+            return "OK - The request has succeeded. The client can read the result of the request in the body and the headers of the response."
+            break;
+        case 201:
+            return "Created - The request has been fulfilled and resulted in a new resource being created."
+            break;
+        case 202:
+            return	"Accepted - The request has been accepted for processing, but the processing has not been completed."
+            break;
+        case 204:
+            return	"No Content - The request has succeeded but returns no message body."
+            break;
+        case 304:
+            return	"Not Modified. See Conditional requests."
+            break;
+        case 400:
+            return	"Bad Request - The request could not be understood by the server due to malformed syntax. The message body will contain more information; see Response Schema."
+            break;
+        case 401:
+            return	"Unauthorized - The request requires user authentication or, if the request included authorization credentials, authorization has been refused for those credentials."
+            break;
+        case 403:
+            return	"Forbidden - The server understood the request, but is refusing to fulfill it."
+            break;
+        case 404:
+            return	"Not Found - The requested resource could not be found. This error can be due to a temporary or permanent condition."
+            break;
+        case 429:
+            return	"Too Many Requests - Rate limiting has been applied."
+            break;
+        case 500:
+            return	"Internal Server Error. You should never receive this error because our clever coders catch them all … but if you are unlucky enough to get one, please report it to us through a comment at the bottom of this page."
+            break;
+        case 502:
+            return	"Bad Gateway - The server was acting as a gateway or proxy and received an invalid response from the upstream server."
+            break;
+        case 503:
+            return	"Service Unavailable - The server is currently unable to handle the request due to a temporary condition which will be alleviated after some delay. You can choose to resend the request again."
+            break;                                                                                                                            
+        default:
+            return	"Unknown Status Code"
+            break;
+    }
+}
